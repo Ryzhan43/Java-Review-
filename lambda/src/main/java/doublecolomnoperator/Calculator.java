f201b1b10c977efe5e74ac2168aa91ab20f6aa4d
Ryzhan43 <ryzhan43@gmail.com>
2023-01-30 18:24:46 -0500

    Learning Double Column Operator

---
+++ lambda/src/main/java/doublecolomnoperator/Calculator.java
@@ -0,0 +1,13 @@
+package doublecolomnoperator;
+
+public class Calculator {
+
+
+    public static  void findSum(int x, int y){
+        System.out.println("Sum " + (x+y));
+    }
+
+    public void findMultiply(int y, int x){
+        System.out.println("Multi[ly " + (x*y));
+    }
+}
---
+++ lambda/src/main/java/doublecolomnoperator/Test.java
@@ -0,0 +1,36 @@
+package doublecolomnoperator;
+
+import java.util.function.BiFunction;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+        //Reference to static method
+        Calculate cl = (x,y) -> Calculator.findSum(x,y);
+        cl.calculate(10,20);
+
+        Calculate cl2 = Calculator::findSum;
+        cl2.calculate(2,2);
+
+        //Reference to instance method
+        Calculate m1 = (x,y) -> new Calculator().findMultiply(x,y);
+        m1.calculate(2,2);
+
+        Calculator obj = new Calculator();
+        Calculate m2 = obj::findMultiply;
+
+        Calculate m3 = new Calculator()::findMultiply;
+
+        BiFunction<String, Integer, String> fn = (srt,i) -> srt.substring(i);
+        System.out.println(fn.apply("Developer", 6));
+
+        BiFunction<String, Integer, String> fn2 = String::substring;
+        System.out.println(fn2.apply("Developer", 6));
+
+
+
+    }
+
+}
