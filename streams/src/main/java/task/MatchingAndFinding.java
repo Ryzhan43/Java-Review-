41de8cedc1cf472bf0b24f3bb0b604ae4f26ad68
Ryzhan43 <ryzhan43@gmail.com>
2023-02-05 13:22:31 -0500

    Starting Stream review

--- streams/src/main/java/task/MatchingAndFinding.java
+++ streams/src/main/java/task/MatchingAndFinding.java
@@ -1,5 +1,8 @@
 package task;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public class MatchingAndFinding {
@@ -33,6 +36,30 @@ public class MatchingAndFinding {
                 .findFirst();
         dish2.stream().forEach(System.out::println);
 
+
+        //REDUCE
+        "************ REDUCE ************\n".transform(System.out::printf);
+        List<Integer> list3 = Arrays.asList(1,2,6,7,8,12,4);
+        list3.stream().reduce(0,(a,b) -> a+b).toString().transform(System.out::printf);
+
+        //REDUCE
+        "************ REDUCE - NO INITIAL VALUE************\n".transform(System.out::printf);
+        Optional<Integer> result =  list3.stream().reduce((a,b) -> a+b);
+        result.stream().forEach(System.out::println);
+
+        //Integer MIN and MAX with REDUCE METHOD
+        Optional<Integer> minInteger = list3.stream().reduce((a,b) -> a < b ? a : b);
+        Optional<Integer> maxInteger = list3.stream().reduce((a,b) -> a > b ? a : b);
+        System.out.println("Min Integer = " + minInteger.get());
+        System.out.println("Max Integer = " + maxInteger.get());
+
+        minInteger = list3.stream().reduce(Integer::min);
+        maxInteger = list3.stream().reduce(Integer::max);
+        Optional<Integer> sumInt = list3.stream().reduce(Integer::sum);
+        System.out.println("2nd coming");
+        System.out.println("Min Integer = " + minInteger.get());
+        System.out.println("Max Integer = " + maxInteger.get());
+        System.out.println("Sum = " + sumInt.get());
 
 
 
